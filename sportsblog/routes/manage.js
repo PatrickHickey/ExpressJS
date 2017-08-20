2fc0dd05015505866242bbd6720de6811e9b1c47
PatrickHickey <patrickstephenhickey@gmail.com>
2017-08-19 19:58:30 -0700

    project7

--- sportsblog/routes/manage.js
+++ sportsblog/routes/manage.js
@@ -2,9 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 Category = require('../models/Category.js');
+Article = require('../models/Article.js');
 
 router.get('/articles', (req, res, next) => {
-  res.render('manage_articles', {title: 'Manage Articles'});
+  Article.getArticles((err, articles) => {
+    if(err){
+      res.send(err);
+    }
+    res.render('manage_articles', {
+      title: 'Manage Articles',
+      articles: articles
+    });
+  });
 });
 
 router.get('/categories', (req, res, next) => {
@@ -21,27 +30,48 @@ router.get('/categories', (req, res, next) => {
 });
 
 router.get('/articles/add', (req, res, next) => {
-  res.render('add_article', {title: 'Create Article'});
+  Category.getCategories((err, categories) => {
+    if(err){
+      res.send(err);
+    }
+    res.render('add_article',
+    {title: 'Create Article',
+    categories: categories});
+  });
 });
 
 router.get('/categories/add', (req, res, next) => {
   res.render('add_category', {title: 'Create Category'});
 });
 
-router.get('/articles/edit/:id', (req, res, next) => {
-  res.render('edit_article', {title: 'Edit Article'});
-});
-
+// Edit Category Page - GET
 router.get('/categories/edit/:id', (req, res, next) => {
   Category.getCategoryById(req.params.id, (err, category) => {
     if(err){
-      res,send(err);
+      res.send(err);
     }
     res.render('edit_category', {
       title: 'Edit Category',
-      category: category
+      category:category
     });
   });
+});
+
+// Edit Article Page - GET
+router.get('/articles/edit/:id', (req, res, next) => {
+  Article.getArticleById(req.params.id, (err, article) => {
+    if(err){
+      res.send(err);
+    }
+    Category.getCategories((err, categories) => {
+      res.render('edit_article', {
+        title: 'Edit Article',
+        article:article,
+        categories: categories
+      });
+    });
+  });
+
 });
 
 module.exports = router;
