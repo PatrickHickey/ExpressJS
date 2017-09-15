affb80e39507be58e5adbc698b9b72dc98ebf11c
PatrickHickey <patrickstephenhickey@gmail.com>
2017-09-15 05:13:17 -0700

    ProjectX

--- storefinder/app.js
+++ storefinder/app.js
@@ -6,6 +6,13 @@ const bodyParser = require('body-parser');
 const NodeGeocoder = require('node-geocoder');
 const redis = require('redis');
 
+// Create redis client
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis Server Connected...');
+});
+
 // Set port
 const port = 3000
 
@@ -26,14 +33,76 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Method-override
 app.use(methodOverride('_method'));
 
-// Search store
+// API options
+const options = {
+  provider: 'google',
+
+  // Optional depending on the providers
+  httpAdapter: 'https', // Default
+  apiKey: 'AIzaSyAJSRtJyEoZnE0sFWlunp-OX_zA5hgU62Q', // for Mapquest, OpenCage, Google Premier
+  formatter: null         // 'gpx', 'string', ...
+};
+
+const geocoder = NodeGeocoder(options);
+
+// Search store page
 app.get('/', (req, res, next) => {
   res.render('searchstore');
 });
 
-// Add Store
+// Add store Page
 app.get('/addstore', (req, res, next) => {
   res.render('addstore');
+});
+
+// Add Store
+app.post('/store/add', (req, res, next) => {
+  const id = req.body.id;
+  const location = req.body.location;
+
+  geocoder.geocode(location)
+    .then((response) => {
+      const store = {};
+      store.lat = response[0].latitude;
+      store.lng = response[0].longitude;
+      store.address = response[0].formattedAddress;
+
+      client.hmset(id, ['lat', store.lat, 'lng', store.lng, 'address', store.address], (err, reply) => {
+        if(err){
+          console.log(err);
+        }
+        console.log(reply);
+        res.redirect('/');
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      return;
+    });
+});
+
+// Search Store
+app.post('/store/search', (req, res, next) => {
+  const id = req.body.id;
+
+  client.hgetall(id, (err, obj) => {
+    if(!obj){
+      res.render('searchstore', {
+        error: 'Invalid Store ID'
+      });
+    } else {
+      obj.id = id;
+      res.render('details', {
+        store: obj
+      });
+    }
+  });
+});
+
+// Delete Store
+app.delete('/store/delete/:id', (req, res, next) => {
+  client.del(req.params.id);
+  res.redirect('/');
 });
 
 app.listen(port, () => {
